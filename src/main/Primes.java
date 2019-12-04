1680a6fa39a753980cb472cd6967eb5c5a8af71d
switchsurfer <GoldenBrown9203172386>
2019-12-04 15:33:47 +0300

    lab2

--- src/main/Primes.java
+++ src/main/Primes.java
@@ -3,21 +3,27 @@ package main;
 public class Primes {
 
     public static void main(String[] args) {
-        for (int i = 1 ; i < 101 ; i++){
-            if(isPrime(i) == true){
+        //перебираем числа от 1 до 100
+        for (int i = 1 ; i <= 100 ; i++){
+            //если число простое выводим его на экран
+            if(isPrime(i)){
                 System.out.println(i);
             }
         }
     }
 
-    public static boolean isPrime(int n) {
+    private static boolean isPrime(int n) {
+        //проверка на то что число положительное
         if (n >= 1) {
-
+            //цикл перебирающий числа от одного до n с шагом 1
             for (int i = 1; i < n; i++) {
+                //проверка на деление без остатка
                 if (i % n == 0) {
                     return false;
                 }
             }
+            //вернем true в случаях когда не попадаем в if который находится в цикле
+            //в случаях когда значение не делится на аргумент без остатка => простое число
             return true;
         }else{
             //ненатуральные числа (отрицательные и 0)
